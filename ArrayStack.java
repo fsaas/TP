a6eb7e466c44d4f09424d90ff12a123aa797a911
fsaas <dododi123@gmail.com>
2017-08-16 21:59:14 +0900

    complete ArrayStack

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,4 +1,64 @@
 public class ArrayStack<Element> {
+    private static final int DEFAULT_MAX_STACK_SIZE = 100;
+    private int _maxSize;
+    private int _top;
+    private Element[] _elements;
+
     public ArrayStack(Element numOfVerticesInGraph) {
+        this._maxSize = (int) numOfVerticesInGraph;
+        this._top = 0;
+        this._elements = (Element[]) new Object[this._maxSize];
+    }
+
+    public boolean isEmpty()
+    {
+        return this._top == 0;
+    }
+    public boolean isFull()
+    {
+        return this._top == this._maxSize;
+    }
+    public int length()
+    {
+        return this._top;
+    }
+    public boolean push(Element anElement)
+    {
+        if(isFull()){
+            return false;
+        }
+        else{
+            this._elements[this._top] = anElement;
+            return true;
+        }
+    }
+    public Element pop()
+    {
+       if(isEmpty()){
+           return null;
+       }else{
+           return this._elements[this._top--];
+       }
+    }
+    public Element peek()
+    {
+        return this._elements[this._top];
+    }
+
+    public ArrayStackIterator arrayStackIterator()
+    {
+        return new ArrayStackIterator();
+    }
+    public class ArrayStackIterator{
+        int _currentPosition;
+
+        public boolean hasNext()
+        {
+           return this._currentPosition < _top;
+        }
+        public Element next()
+        {
+           return _elements[_top];
+        }
     }
 }
